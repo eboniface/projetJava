4ba65e199a9a42eb420480954110c2837de1631a
Hugo MASSON <hugo.masson@epsi.fr>
2020-12-15 14:28:55 +0100

    mattéo

--- src/main/java/com/example/ProjetJEE/HelloServlet.java
+++ src/main/java/com/example/ProjetJEE/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "Mattéo n'a pas d'amis!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
